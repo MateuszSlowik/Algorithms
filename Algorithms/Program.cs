28929c57fcca4bc85d3ad668a8f4452f0aebe044
Mateusz <slowikmat99@gmail.com>
2022-07-23 12:16:05 +0200

    Two sum algorithm added

---
+++ Algorithms/Program.cs
@@ -0,0 +1,13 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using Algorithms;
+
+int[] nums = { -2, 0, -123, 324, 2, 15, 7, 11, 15 };
+int target = 339;
+
+TwoSunsAlgorithm.CalculateTwoSunsAlgorithm(target, nums);
+
+
+
+
+
---
+++ Algorithms/TwoSunsAlgorithm.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Algorithms
+{
+    public static class TwoSunsAlgorithm
+    {
+        private static int target;
+        private static List<int> numbers = new List<int>();
+        private static string result = "";
+
+        public static void CalculateTwoSunsAlgorithm(int _target, int[] _numbers)
+        {
+            target = _target;
+            numbers = _numbers.ToList();
+            result = CalculateResult();
+
+            DisplayResult();
+        }
+
+
+
+        private static string CalculateResult()
+        {
+            for (int i = 0; i < numbers.Count; i++)
+            { 
+                numbers.Select(x => x != numbers[i]);
+               
+                for (int j = 0; j < numbers.Count; j++)
+                {
+                    if (numbers[i] + numbers[j] == target)
+                    {
+                        return $"[{numbers[i]}, {numbers[j]}]";
+                    }
+
+                }
+
+            }
+            return "No result found";
+        }
+
+
+
+        private static void DisplayResult()
+        {
+            Console.WriteLine($"The result for [{string.Join(", ", numbers)}] and target: {target} is: {result}");
+        }
+
+    }
+}
